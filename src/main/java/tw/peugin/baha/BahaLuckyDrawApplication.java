2cab9ccd080a8ab7d42b8c50008584234aecfbb2
Peugin <ColdPeugin@gmail.com>
2021-05-24 18:59:53 +0800

    程式內部調整，只移動了位置。

---
+++ src/main/java/tw/peugin/baha/BahaLuckyDrawApplication.java
@@ -0,0 +1,13 @@
+package tw.peugin.baha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahaLuckyDrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BahaLuckyDrawApplication.class, args);
+    }
+
+}

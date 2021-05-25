dcdcc16030c12045c1ff5c80dee16ae84eb92452
Peugin <ColdPeugin@gmail.com>
2021-05-25 08:57:04 +0800

    調整參數。

--- src/main/java/tw/peugin/heroku/db/DatabaseConfig.java
+++ src/main/java/tw/peugin/heroku/db/DatabaseConfig.java
@@ -10,11 +10,17 @@ public class DatabaseConfig {
 
     @Value("${SPRING_DATASOURCE_URL}")
     private String dbUrl;
+    @Value("${SPRING_DATASOURCE_USERNAME}")
+    private String userName;
+    @Value("${SPRING_DATASOURCE_PASSWORD}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+        config.setUsername(userName);
+        config.setPassword(password);
         return new HikariDataSource(config);
     }
 }

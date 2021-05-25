e0d6c4a8aae3b25f6684dc5ffe5c7cd3226e5f68
Peugin <ColdPeugin@gmail.com>
2021-05-25 08:59:56 +0800

    調整參數。

--- src/main/java/tw/peugin/baha/BahaLuckyDrawApplication.java
+++ src/main/java/tw/peugin/baha/BahaLuckyDrawApplication.java
@@ -3,7 +3,7 @@ package tw.peugin.baha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "tw.peugin.heroku.db")
 public class BahaLuckyDrawApplication {
 
     public static void main(String[] args) {

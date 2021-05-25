1e64dfdf16149dccaa34eac947b67cce9d2d954e
Peugin <ColdPeugin@gmail.com>
2021-05-25 12:04:58 +0800

    調整參數。

--- src/main/java/tw/peugin/baha/BahaLuckyDrawApplication.java
+++ src/main/java/tw/peugin/baha/BahaLuckyDrawApplication.java
@@ -6,19 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BahaLuckyDrawApplication {
-    @Value("${spring.datasource.password}")
-    private static String password;
-    @Value("${spring.datasource.username}")
-    private static String userName;
-
     public static void main(String[] args) {
-        System.out.println("un:" + userName);
-        System.out.println("pwd:" + password);
-        System.out.println("env:" + System.getenv("SPRING_DATASOURCE_PASSWORD"));
         SpringApplication.run(BahaLuckyDrawApplication.class, args);
-        System.out.println("un:" + userName);
-        System.out.println("pwd:" + password);
-        System.out.println("env:" + System.getenv("SPRING_DATASOURCE_PASSWORD"));
     }
 
 }

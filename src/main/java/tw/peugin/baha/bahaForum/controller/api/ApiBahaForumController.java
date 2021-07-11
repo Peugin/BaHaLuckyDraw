97cd0b950bf53eaeafb20f5cf72286a6c72965cb
Peugin <ColdPeugin@gmail.com>
2021-07-11 16:18:34 +0800

    現在會依照狀態，自行使用電腦或手機版網頁爬蟲。

--- src/main/java/tw/peugin/baha/bahaForum/controller/api/ApiBahaForumController.java
+++ src/main/java/tw/peugin/baha/bahaForum/controller/api/ApiBahaForumController.java
@@ -1,15 +1,17 @@
 package tw.peugin.baha.bahaForum.controller.api;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.InternalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import tw.peugin.baha.bahaForum.BahaFormCrawler;
+import tw.peugin.baha.bahaForum.BahaMobileForumCrawler;
+import tw.peugin.baha.bahaForum.BahaPCForumCrawler;
+import tw.peugin.baha.bahaForum.IForumCrawler;
 import tw.peugin.baha.bahaForum.entity.BahaCrawlerData;
 import tw.peugin.baha.luckydraw.exception.ArgumentInvalidException;
+import tw.peugin.baha.utility.HtmlHelper;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -22,7 +24,9 @@ import java.util.regex.Pattern;
 public class ApiBahaForumController {
     private final Pattern bahaCPagePattern = Pattern.compile("https://(forum\\.gamer\\.com\\.tw|m\\.gamer\\.com\\.tw/forum)/C\\.php\\?.+");
     @Autowired
-    private BahaFormCrawler bahaFormCrawler;
+    private BahaPCForumCrawler bahaPCForumCrawler;
+    @Autowired
+    private BahaMobileForumCrawler bahaMobileFormCrawler;
 
     @RequestMapping(value = "/getPosters")
     public List<BahaCrawlerData> getPosters (@RequestParam(name="url") String url,
@@ -30,8 +34,6 @@ public class ApiBahaForumController {
                                              @RequestParam(name="end_floor",required = false) Integer endFloor,
                                              @RequestParam(name="start_date",required = false) @DateTimeFormat(pattern="yyyy-MM-dd HH:mm") Date startDate,
                                              @RequestParam(name="end_date",required = false) @DateTimeFormat(pattern="yyyy-MM-dd HH:mm") Date endDate) throws ArgumentInvalidException, IOException, URISyntaxException {
-        Document doc = Jsoup.connect("https://forum.gamer.com.tw/Co.php?bsn=60076&sn=71336748").userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0").ignoreContentType(true).get();
-
         if(!bahaCPagePattern.matcher(url).find())
             throw new ArgumentInvalidException("輸入的網址不正確");
         if(endFloor != null && endFloor < 0 || (startFloor != null &&  endFloor != null && (endFloor > 0 && endFloor < startFloor)))
@@ -44,8 +46,19 @@ public class ApiBahaForumController {
         if(startDate == null) startDate = new Date(Long.MIN_VALUE);
         if(endDate == null) endDate = new Date(Long.MAX_VALUE);
 
-        url = url.replace("https://m.gamer.com.tw/forum/","https://forum.gamer.com.tw/");
 
-        return bahaFormCrawler.scraping(url,startFloor,endFloor,startDate,endDate);
+        switch (HtmlHelper.testHttpStatus()){
+            case PC:
+                System.out.println("使用PC版爬蟲");
+                url = url.replace("https://m.gamer.com.tw/forum/","https://forum.gamer.com.tw/");
+                return bahaPCForumCrawler.scraping(url,startFloor,endFloor,startDate,endDate);
+            case Mobile:
+                System.out.println("使用Mobile版爬蟲");
+                url = url.replace("https://forum.gamer.com.tw/","https://m.gamer.com.tw/forum/");
+                return bahaMobileFormCrawler.scraping(url,startFloor,endFloor,startDate,endDate);
+            default:
+                System.out.println("無法爬蟲");
+                throw new InternalException("無法連線至巴哈姆特，請稍後再試。");
+        }
     }
 }
